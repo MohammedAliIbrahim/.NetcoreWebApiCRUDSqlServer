0592e5de710bae14f67a06533af8d2c0ef1fd0c2
MohammedAliIbrahim <mhalieg@yahoo.com>
2019-02-12 22:19:46 +0300

    reupdate

---
+++ LeadWebAreas/Areas/Product/Models/DatabaseContext.cs
@@ -0,0 +1,46 @@
+﻿using LeadCoreAreas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeadCoreAreas.Areas.Product.Models
+{
+    public class DatabaseContext : DbContext
+    {
+
+
+
+        /*   public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+       { }*/
+
+        /*   private readonly IConfiguration configuration;
+
+           public DatabaseContext( IConfiguration config)
+           {
+               this.configuration = config;
+           }*/
+
+        //  String constring = configuration.GetConnectionString("DefaultConnection");
+
+        /*  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+          {
+              optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+          }*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=dotnetest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+          //  optionsBuilder.UseSqlServer(@"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dotnetest;Integrated Security=True;");
+        }
+        //entities
+        public DbSet<store> Stores { get; set; }
+        public DbSet<product> Products { get; set; }
+
+
+
+
+    }
+}
---
+++ LeadWebAreas/Areas/Utils/Common.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace LeadCoreAreas.Areas
+{
+    public class CommonResponse
+    {
+        protected internal HttpContext Current => new HttpContextAccessor().HttpContext;
+      
+        public CommonResponse(int status,string message, object data=null)
+        {
+            
+            this.Status = status;
+            this.Message = message;
+            this.Data = data;
+            
+
+        }
+        public int Total { get; set; }
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public dynamic Data { get; set; }
+    }
+
+    public class PagingFilter
+    {
+        public string Query { get; set; }
+        public int? ID { get; set; }
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+        public string OrderByDirection { get; set; } 
+        public string OrderBy { get; set; }
+    }
+
+}
